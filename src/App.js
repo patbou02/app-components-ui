7c80e1ebe48ce3634ef4f0a7b8d7c945aabecd0c
Patrick Boutin <patrick.boutin@roberthalf.com>
2023-12-29 01:23:10 -0800

    Created new Button component, imported it into App component, Set props to be boolean and installed PropTypes package to be used to validate the prop type based on boolean and presence. If count > 1 that means more than one prop set to true therefore throw an Error.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Button from './Button';
+
 function App() {
-  return <h1>App</h1>;
+  return (
+    <div>
+      <h1>App</h1>
+      <div><Button outline rounded>Click me!</Button></div>
+      <div><Button primary  outline rounded>Buy Now!</Button></div>
+      <div><Button secondary outline rounded>See Deal!</Button></div>
+      <div><Button warning outline rounded>Hide Ads!</Button></div>
+      <div><Button danger rounded>Something...</Button></div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,30 @@
+import propTypes from 'prop-types';
+
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded
+}) {
+  return <button>{children}</button>;
+}
+
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    const count = Number(!!primary)
+      + Number(!!secondary)
+      + Number(!!success)
+      + Number(!!warning)
+      + Number(!!danger);
+    if (count > 1) {
+      return new Error('Only one of primary, secondary, success, warning or danger.');
+    }
+  }
+
+};
+
+export default Button;
