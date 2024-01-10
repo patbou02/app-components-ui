4aa869576930a246baa519653de8616ebb774ebb
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-10 01:06:12 -0500

    Installed TailwindCSS, PostCSS and PostCSS packages and set up in project. Set default basic classes to be used in the returned <button> element from the Button component.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div>
       <h1>App</h1>
       <div><Button outline rounded>Click me!</Button></div>
-      <div><Button primary  outline rounded>Buy Now!</Button></div>
+      <div><Button primary outline rounded>Buy Now!</Button></div>
       <div><Button secondary outline rounded>See Deal!</Button></div>
       <div><Button warning outline rounded>Hide Ads!</Button></div>
       <div><Button danger rounded>Something...</Button></div>
