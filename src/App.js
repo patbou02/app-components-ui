17fa56052be9b35656bc7e2bf8bd4c2a6aec2edb
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-10 01:47:19 -0500

    Installed 'react-icons' package, added a few simple icons to demo page and tweaked classes to account for presence of icons via vanilla CSS and flex classes within the classes variable using tailwindCSS.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { GoBell, GoCloudOffline, GoDatabase } from 'react-icons/go';
 import Button from './Button';
 
 function App() {
   return (
     <div>
       <h1>App</h1>
-      <div><Button primary outline rounded>Click me!</Button></div>
+      <div><Button primary outline rounded>
+        <GoBell />
+        Click me!
+      </Button></div>
       <div><Button secondary outline rounded>Buy Now!</Button></div>
-      <div><Button success outline rounded>See Deal!</Button></div>
+      <div><Button success outline rounded>
+        <GoCloudOffline />
+        See Deal!</Button></div>
       <div><Button warning outline rounded>Hide Ads!</Button></div>
-      <div><Button danger rounded>Something...</Button></div>
+      <div><Button danger outline rounded>
+        <GoDatabase />
+        Something...</Button></div>
     </div>
   );
 }
