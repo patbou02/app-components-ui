a7b3dfb8308d435e10985c36070ab8de52c0f2ff
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-10 23:11:26 -0500

    Added inline classes to Button elements and enhanced component using spread operator so that any other non specifically named prop gets passed down to the button html element or the classes variable.

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ function App() {
   return (
     <div>
       <h1>App</h1>
-      <div><Button primary outline rounded>
+      <div><Button className="mb-5" primary outline rounded>
         <GoBell />
         Click me!
       </Button></div>
-      <div><Button secondary outline rounded>Buy Now!</Button></div>
-      <div><Button success outline rounded>
+      <div><Button className="mb-5" secondary outline rounded>Buy Now!</Button></div>
+      <div><Button className="mb-5" success outline rounded>
         <GoCloudOffline />
         See Deal!</Button></div>
-      <div><Button warning outline rounded>Hide Ads!</Button></div>
-      <div><Button danger outline rounded>
+      <div><Button className="mb-5" warning outline rounded>Hide Ads!</Button></div>
+      <div><Button className="mb-5" danger outline rounded>
         <GoDatabase />
         Something...</Button></div>
     </div>
--- src/Button.js
+++ src/Button.js
@@ -8,15 +8,19 @@ function Button({
   warning,
   danger,
   outline,
-  rounded
+  rounded,
+  ...rest
 }) {
-  const classes = className('flex items-center px-3 py-1.5 border', {
-    'border-blue-500 bg-blue-500 text-white': primary,
-    'border-gray-900 bg-gray-900 text-white': secondary,
-    'border-green-500 bg-green-500 text-white': success,
-    'border-yellow-400 bg-yellow-400 text-white': warning,
-    'border-red-500 bg-red-500 text-white': danger,
+  const classes = className(
+    rest.className,
+    'flex items-center px-3 py-1.5 border', {
+    'border-blue-500 bg-blue-500': primary,
+    'border-gray-900 bg-gray-900': secondary,
+    'border-green-500 bg-green-500': success,
+    'border-yellow-400 bg-yellow-400': warning,
+    'border-red-500 bg-red-500': danger,
     'rounded-full': rounded,
+    'text-white': !outline && (primary || secondary || success || warning || danger),
     'bg-white': outline,
     'text-blue-500': outline && primary,
     'text-gray-900': outline && secondary,
@@ -25,7 +29,7 @@ function Button({
     'text-red-500': outline && danger,
   });
 
-  return <button className={classes}>{children}</button>;
+  return <button {...rest} className={classes}>{children}</button>;
 }
 
 Button.propTypes = {
