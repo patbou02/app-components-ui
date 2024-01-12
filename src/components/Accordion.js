13c096622fe1d1e5a37c2f742940397dde689469
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-11 23:35:37 -0500

    Added click handler function and passed index down to it.

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -5,13 +5,15 @@ function Accordion({ items }) {
 
   const [expandedIndex, setExpandedIndex] = useState(0);
 
+  const handleClick = nextIndex => setExpandedIndex(nextIndex);
+
   const renderedItems = items.map((item, i) => {
 
     const isExpanded = (i === expandedIndex);
 
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
+        <div onClick={() => handleClick(i)}>{item.label}</div>
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
