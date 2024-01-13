d4180ca59f72ad1a886d5a08eca06b69678dbe06
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-12 19:29:17 -0500

    Refactored out duplicate code/structure and placed into individual Panel component.

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { GoChevronDown, GoChevronUp } from 'react-icons/go';
+import Panel from './Panel';
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,17 +12,17 @@ function Dropdown({ options, value, onChange }) {
     onChange(option);
   };
 
-  console.log(options, value, onChange);
-
   const renderedOptions = options.map(option => {
     return <div className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={() => handleOptionClick(option)} key={option.value} value={option.value}>{option.label}</div>
   });
 
   return (
     <div className="w-48 relative">
-      <div className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full" onClick={handleClick}>{value?.label || 'Select...'}
-        {isOpen? <GoChevronUp className="text-lg" /> : <GoChevronDown className="text-lg" />}</div>
-      {isOpen && <div className="absolute top-full border rounded p-3 shadow bg-white w-full">{renderedOptions}</div>}
+      <Panel className="flex justify-between items-center cursor-pointer" onClick={handleClick}>
+        {value?.label || 'Select...'}
+        {isOpen? <GoChevronUp className="text-lg" /> : <GoChevronDown className="text-lg" />}
+      </Panel>
+      {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
     </div>
   );
 }
