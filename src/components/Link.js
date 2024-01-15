9688e5b90d9ab5a16ca3c3ef6e0612af212b2368
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-15 00:46:35 -0500

    Created small custom hook called useNaviation() and refactored duplicated code from both Link.js and Route.js.

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,9 +1,11 @@
-import { useContext } from 'react';
-import NavigationContext from '../context/navigation';
+import useNavigation from '../hooks/use-navigation';
+import classNames from 'classnames';
 
 function Link({ to, children }) {
 
-  const { navigate } = useContext(NavigationContext);
+  const { navigate } = useNavigation();
+
+  const classes = classNames('text-blue-500');
 
   const handleClick = event => {
 
@@ -13,7 +15,7 @@ function Link({ to, children }) {
     navigate(to);
   };
 
-  return <a href={to} onClick={handleClick}>{ children }</a>;
+  return <a className={classes} href={to} onClick={handleClick}>{ children }</a>;
 }
 
 export default Link;
