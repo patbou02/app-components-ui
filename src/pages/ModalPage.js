704b666de89e941f010628b8637d81d3d3ccc365
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-15 12:35:37 -0500

    Added dummy content to the ModalPage component. In the Modal component, use useEffect on initial render to add a TailwindCSS overflow hidden rule on modal render and remove when closing modal to prevent scrolling. Added additional styling around children and actionBar to properly position elements.

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -25,6 +25,14 @@ function ModalPage() {
     <div>
       <Button primary onClick={handleClick}>Open Modal</Button>
       {showModal && modal}
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aliquid cumque distinctio eum explicabo fugit hic libero magnam magni modi nesciunt nobis officia, officiis omnis quasi quidem recusandae sequi sunt.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab corporis debitis, dolores eius error ex excepturi explicabo id labore odit pariatur quos rem reprehenderit temporibus voluptas. Harum nihil numquam ratione.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A dolorem ea excepturi ipsa molestias officia, rem repudiandae suscipit vero? Animi deserunt dicta hic non odit, officiis quibusdam ratione reiciendis voluptas?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, excepturi unde? Ad aliquid corporis et fugiat ipsam nesciunt quis tempora totam! Cupiditate, ipsa maiores. Blanditiis ducimus eius ex laboriosam tempora!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid asperiores cum et expedita fugit inventore, ipsum itaque iusto laboriosam, maxime minus nemo, obcaecati perspiciatis sit vitae voluptatem! Dicta, nisi!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur consequuntur deserunt dolorem doloribus hic inventore ipsam iure magni nesciunt nisi placeat quod repellat reprehenderit rerum similique unde ut, voluptatem.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus adipisci autem culpa doloremque ducimus, earum excepturi in magnam maxime, modi neque pariatur quae quidem similique voluptatum. Laboriosam molestiae quia quibusdam.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cupiditate dolores, eum in quidem quod similique tempora totam. Animi illo libero magnam necessitatibus numquam quam quisquam reprehenderit soluta tempora voluptatem.</p>
     </div>
   );
 }
