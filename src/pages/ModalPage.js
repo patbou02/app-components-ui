4a03e4c72fb2162e5561509d0e0225d3c1349623
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-15 12:17:09 -0500

    Updated Modal to include children and actionBar props which are passed down from the ModalPage component along with click handlers to make sure modal closes on click of the action bar button.

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -9,10 +9,22 @@ function ModalPage() {
 
   const handleClose = () => setShowModal(false);
 
+  const actionBar = (
+    <div>
+      <Button onClick={handleClose} primary>I accept</Button>
+    </div>
+  );
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium harum iure labore, nesciunt quia quisquam repellendus reprehenderit sapiente sed ullam. Iste nam neque numquam veniam? Eius laborum nesciunt quisquam voluptate.</p>
+    </Modal>
+  );
+
   return (
     <div>
       <Button primary onClick={handleClick}>Open Modal</Button>
-      {showModal && <Modal onClose={handleClose} />}
+      {showModal && modal}
     </div>
   );
 }
