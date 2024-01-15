7802303f2e8ca22b4f28955c133ba8de0012e073
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-15 17:57:30 -0500

    Created dummy data array which is then passed down to Table as a data Prop. Then in Table, mapped through the data array in return table rows with appropriate information to then be rendered in the table body.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,29 @@
-function Table() {
-  return <div>Table</div>;
+function Table({ data }) {
+
+  const renderedRows = data.map(fruit => {
+    return (
+      <tr key={fruit.name}>
+        <td>{fruit.name}</td>
+        <td>{fruit.color}</td>
+        <td>{fruit.score}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <table>
+      <thead>
+      <tr>
+        <th>Fruit</th>
+        <th>Color</th>
+        <th>Score</th>
+      </tr>
+      </thead>
+      <tbody>
+      {renderedRows}
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
