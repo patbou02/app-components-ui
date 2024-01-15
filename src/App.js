aa4b712bd65ebe8a6a3c81e177fe1fc57484c022
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-15 00:23:32 -0500

    Created Route component to handle basic navigation by using the navigation context. Added the Route to the App component and set 3 routes with the existing component page files.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import Link from './components/Link';
+import Route from './components/Route';
+import AccordionPage from './pages/AccordionPage';
+import ButtonPage from './pages/ButtonPage';
+import DropdownPage from './pages/DropdownPage';
 
 function App() {
 
@@ -7,6 +11,17 @@ function App() {
       <Link to="/accordion">Go to Accordion page</Link>
       <Link to="/button">Go to Button page</Link>
       <Link to="/dropdown">Go to Dropdown page</Link>
+      <div>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/button">
+          <ButtonPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
+      </div>
     </div>
   );
 }
---
+++ src/components/Route.js
@@ -0,0 +1,11 @@
+import { useContext } from 'react';
+import NavigationContext from '../context/navigation';
+
+function Route({ path, children }) {
+
+  const { currentPath } = useContext(NavigationContext);
+
+  return (path === currentPath) ? children : null;
+};
+
+export default Route;
