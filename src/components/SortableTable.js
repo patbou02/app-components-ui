6d2cd4fa90f51727ce8fb74017b41cc9e5c7a188
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-17 00:24:23 -0500

    Added icons and necessary styling for correct placement.

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Table from './Table';
+import { GoTriangleDown, GoTriangleUp } from "react-icons/go";
 
 function SortableTable(props) {
   const [sortOrder, setSortOrder] = useState(null);
@@ -26,7 +27,14 @@ function SortableTable(props) {
     } else {
       return {
         ...column,
-        header: () => <th onClick={() => handleClick(column.label)}>{column.label} IS SORTABLE</th>,
+        header: () => (
+          <th className="cursor-pointer hover:bg-gray-100" onClick={() => handleClick(column.label)}>
+            <div className="flex items-center">
+              {getIcons(column.label, sortBy, sortOrder)}
+              {column.label}
+            </div>
+          </th>
+        ),
       };
     }
   });
@@ -52,10 +60,40 @@ function SortableTable(props) {
   }
 
   return (
-    <div> {sortOrder} - {sortBy}
     <Table {...props} data={sortedData} config={updatedConfig} />
-    </div>
   );
+}
+
+function getIcons(label, sortBy, sortOrder) {
+  if (label !== sortBy) {
+    return (
+      <div>
+        <GoTriangleUp />
+        <GoTriangleDown />
+      </div>
+    );
+  }
+
+  if (sortOrder === null) {
+    return (
+      <div>
+        <GoTriangleUp />
+        <GoTriangleDown />
+      </div>
+    );
+  } else if (sortOrder === 'asc') {
+    return (
+      <div>
+        <GoTriangleUp />
+      </div>
+    );
+  } else if (sortOrder === 'desc') {
+    return (
+      <div>
+        <GoTriangleDown />
+      </div>
+    );
+  }
 }
 
 export default SortableTable;
