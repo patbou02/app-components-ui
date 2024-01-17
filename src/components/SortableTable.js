75142c72bf1679797026dc4ef88cca9db879d5af
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-16 23:39:21 -0500

    Created click handler function and added it to the header th tag.

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -3,13 +3,15 @@ import Table from './Table';
 function SortableTable(props) {
   const { config } = props;
 
+  const handleClick = label => console.log(label)
+
   const updatedConfig = config.map(column => {
     if (!column.sortValue) {
       return column;
     } else {
       return {
         ...column,
-        header: () => <th>{column.label} IS SORTABLE</th>,
+        header: () => <th onClick={() => handleClick(column.label)}>{column.label} IS SORTABLE</th>,
       };
     }
   });
